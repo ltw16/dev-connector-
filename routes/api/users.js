0f6400e6980db9d496ade1a530f74b32d1568216
Liam Webb <liamwebb11@hotmail.com>
2018-09-18 19:32:08 +0000

    Added additional registration and login rules

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
@@ -68,15 +69,24 @@ router.post("/register", (req, res) => {
 //@desc  Login user / Returning JWT Token  
 //@access Public
 router.post("/login", (req, res) => {
-     const email = req.body.email;
-     const password = req.body.password;
+    
+    const { errors, isValid } = validateLoginInput(req.body);
+    
+    // Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    
+    const email = req.body.email;
+    const password = req.body.password;
      
      // Find user by email
      User.findOne({email: email})
         .then(user => {
             // Check for user
             if(!user) {
-                return res.status(404).json({email: "User not found"});
+                errors.email = "User not found";
+                return res.status(404).json(errors);
             } 
             
             // Check Password
@@ -100,7 +110,8 @@ router.post("/login", (req, res) => {
                                 });
                         });
                     } else {
-                        return res.status(400).json({password: "Password incorrect"});
+                        errors.password = "Password Incorrect";
+                        return res.status(400).json(errors);
                     }
                 });
         })
