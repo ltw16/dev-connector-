cb8bf745bd1aaa90045712a6fbc46f2ce1f0764c
Liam Webb <liamwebb11@hotmail.com>
2018-09-23 16:21:35 +0000

    Added Registration & Error reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,16 @@
-import { TEST_DISPATCH } from "./types";
-
+import { GET_ERRORS } from "./types";
+import axios from "axios";
 
 // Register User
-export const registerUser = (userData) => {
-    return {
-        type: TEST_DISPATCH,
-        payload: userData
-    }
+export const registerUser = (userData, history) => dispatch => {
+    axios
+    .post("https://devconnector-liamwebb.c9users.io/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    );
+
 }
