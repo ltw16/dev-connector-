5b94731b8c9ea7035e3b5ebd0c3afc62d1f96efa
Liam Webb <liamwebb11@hotmail.com>
2018-09-23 20:31:36 +0000

    Added Redux Login Action & Setting Current User

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,9 @@
+import isEmpty from "../validation/is-empty";
 
- 
+import { SET_CURRENT_USER } from "../actions/types";
+
+
+
 const initialState = {
     isAuthenticated: false,
     user: {}
@@ -7,7 +11,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
-        
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+            }
         default: 
             return state;
     }
