02b2447f15e599efe5e97adfa3911f84659ebb5b
Liam Webb <liamwebb11@hotmail.com>
2018-10-01 12:45:24 +0000

    Added Delete, Like & Unlike Posts functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -4,7 +4,8 @@ import {
     ADD_POST,
     GET_ERRORS,
     GET_POSTS,
-    POST_LOADING
+    POST_LOADING,
+    DELETE_POST
 } from "./types";
 
 // Add Post
@@ -40,6 +41,50 @@ export const getPosts = () => dispatch => {
             dispatch({
                 type: GET_POSTS,
                 payload: null
+            })
+        );
+}
+
+// Delete Post
+export const deletePost = id => dispatch => {
+    axios
+        .delete(`https://devconnector-liamwebb.c9users.io/api/posts/${id}`)
+        .then(res => 
+            dispatch({
+                type: DELETE_POST,
+                payload: id
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
+
+// Add Like
+export const addLike = id => dispatch => {
+    axios
+        .post(`https://devconnector-liamwebb.c9users.io/api/posts/like/${id}`)
+        .then(res => dispatch(getPosts()))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
+
+// Remove Like
+export const removeLike = id => dispatch => {
+    axios
+        .post(`https://devconnector-liamwebb.c9users.io/api/posts/unlike/${id}`)
+        .then(res => dispatch(getPosts()))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
             })
         );
 }
